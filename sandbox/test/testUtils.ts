e335758803a56bb0e225766dc18d7adf865a57c1
Sugit <sgmt.snj@gmail.com>
2022-03-01 15:32:28 +0000

    chore: stuff

--- sandbox/test/testUtils.ts
+++ sandbox/test/testUtils.ts
@@ -1,25 +1,25 @@
-import { render } from '@testing-library/react'
-import React from 'react'
-// import { ThemeProvider } from "my-ui-lib"
-// import { TranslationProvider } from "my-i18n-lib"
-// import defaultStrings from "i18n/en-x-default"
-
-const Providers = ({ children }) => {
-  return children
-  // return (
-  //   <ThemeProvider theme="light">
-  //     <TranslationProvider messages={defaultStrings}>
-  //       {children}
-  //     </TranslationProvider>
-  //   </ThemeProvider>
-  // )
-}
-
-const customRender = (ui: React.ReactElement, options = {}): any =>
-  render(ui, { wrapper: Providers, ...options })
-
+// import { render } from '@testing-library/react'
+// import React from 'react'
+// // import { ThemeProvider } from "my-ui-lib"
+// // import { TranslationProvider } from "my-i18n-lib"
+// // import defaultStrings from "i18n/en-x-default"
+//
+// const Providers = ({ children }) => {
+//   return children
+//   // return (
+//   //   <ThemeProvider theme="light">
+//   //     <TranslationProvider messages={defaultStrings}>
+//   //       {children}
+//   //     </TranslationProvider>
+//   //   </ThemeProvider>
+//   // )
+// }
+//
+// const customRender = (ui: React.ReactElement, options = {}): any =>
+//   render(ui, { wrapper: Providers, ...options })
+//
 // re-export everything
 export * from '@testing-library/react'
-
-// override render method
-export { customRender as render }
+//
+// // override render method
+// export { customRender as render }
