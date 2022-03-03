c3f28525aaedc3d15ab13b566c4e135c5fffaef1
Sugit <sgmt.snj@gmail.com>
2022-03-03 14:41:16 +0000

    chore: update configs to enable the github actions

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : '';
+
 const nextConfig = {
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
+  trailingSlash: true,
   reactStrictMode: true,
 };
 
