a4c7d808db7830485226509b7c5888ad7fc8c9d2
Sugit <sgmt.snj@gmail.com>
2022-03-04 17:19:06 +0000

    chore: introduce nextui

---
+++ client/components/theme-switch.tsx
@@ -0,0 +1,18 @@
+import { useTheme as useNextTheme } from 'next-themes';
+import { Switch, useTheme } from '@nextui-org/react';
+
+const ThemeSwitch = () => {
+  const { setTheme } = useNextTheme();
+  const { isDark, type } = useTheme();
+
+  return (
+    <>
+      <Switch
+        checked={isDark}
+        onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
+      />
+    </>
+  );
+};
+
+export default ThemeSwitch;
